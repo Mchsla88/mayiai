4fab03d7a2a08f991cb6ab5fc85a506c03597a92
Mchsla88 <mchsla88@gmail.com>
2025-12-05 14:58:05 +0100

    Install Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -110,6 +110,10 @@ export const metadata: Metadata = {
   },
 }
 
+import Script from 'next/script'
+
+// ... existing imports
+
 export default function RootLayout({
   children,
 }: {
@@ -118,6 +122,23 @@ export default function RootLayout({
   return (
     <html lang="pl" className={inter.variable} suppressHydrationWarning>
       <body className={`${inter.className} antialiased`}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PW3756XX"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PW3756XX');
+          `}
+        </Script>
         <OrganizationStructuredData />
         <EducationalOrganizationStructuredData />
         <WebsiteStructuredData />
